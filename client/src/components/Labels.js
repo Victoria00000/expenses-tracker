19a30af1c4a2bdafb935c292b6654de80b0f7a9d
Victoria00000 <vvictoriaandres@gmail.com>
2022-07-29 12:51:09 -0300

    Label.js component added, boxicons installed

--- client/src/components/Labels.js
+++ client/src/components/Labels.js
@@ -2,17 +2,17 @@
 const obj = [
     {
         type: 'Savings',
-        color: '#f9c74f',
+        color: 'rgb(255, 99, 132)',
         percent: 45
     },
     {
         type: 'Investment',
-        color: '#f9c74f',
+        color: 'rgb(54, 162, 235)',
         percent: 20
     },
     {
         type: 'Expense',
-        color: '',
+        color: 'rgb(255, 205, 86)',
         percent: null
     }
 ];
@@ -30,7 +30,7 @@ const LabelComponent = ({ data }) => {
     return (
         <div className="labels flex justify-between">
             <div className="flex gap-2">
-                <div className="w-2 h-2 rounded py-3" style={{ background: data.color || '#9d4edd' }}></div>
+                <div className="w-2 h-2 rounded py-3" style={{ background: data.color || 'grey' }}></div>
                 <h3 className="text-md"> {data.type || '-'} </h3>
             </div>
             <h3 className="font-bold"> {data.percent || '-'} % </h3>
--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,9 +1,37 @@
+import 'boxicons';
 
+
+const obj = [
+    {
+        name: 'Savings',
+        color: 'rgb(255, 99, 132)',
+    },
+    {
+        name: 'Investment',
+        color: 'rgb(54, 162, 235)',
+    },
+    {
+        name: 'Expense',
+        color: 'rgb(255, 205, 86)',
+    }
+];
 
 export const List = () => {
     return (
-        <div>
-            List
+        <div className='flex flex-col py-6 gap-3'>
+            <h1 className='py-4 font-bold text-xl'> History </h1>
+            {obj.map((v, i) => <Transactions category={v} key={i} />)}
+        </div>
+    );
+};
+
+const Transactions = ({ category }) => {
+    if (!category) { return null };
+    return (
+        <div className='item flex justify-center bg-gray-50 py-2 rounded-r' style={{ borderRight: `8px solid ${category.color || 'grey'}` }}>
+            <button className='px-3'><box-icon name='trash' size='15px' color={category.color || 'grey'}></box-icon></button>
+            <span className='block w-full'> {category.name || '-'} </span>
+
         </div>
     );
 };
